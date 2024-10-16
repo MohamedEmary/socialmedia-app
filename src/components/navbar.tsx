69690c6ec7bdc2aa714150cfc6529a89a55a92b9
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-10-17 01:42:19 +0300

    feat: change navbar content based on authToken, current path
    If the user is not logged in (token doesn't exist) show login button
    if we are in signup page and signup if we are in login page
    
    If the user is logged in (token exist) show user avatar with profile and logout opts

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,18 +18,23 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import reduxStore, { RootState } from "@/lib/Redux/ReduxStore";
 import { getMyData } from "@/lib/Redux/PostsSlice";
+import { usePathname } from "next/navigation";
+import { clearUserData } from "@/lib/Redux/AuthSlice";
 
 export function Navbar() {
   const dispatch = useDispatch<typeof reduxStore.dispatch>();
   const token = useSelector((state: RootState) => state.auth.userToken);
+  const myInfo = useSelector((state: RootState) => state.posts.myInfo);
 
-  const myInfo: myInfo = useSelector(
-    (state: ReturnType<typeof reduxStore.getState>) => state.posts.myInfo
-  );
+  const currentPath = usePathname();
+
+  const handleLogout = () => {
+    dispatch(clearUserData());
+  };
 
   useEffect(() => {
     if (token) {
-    dispatch(getMyData());
+      dispatch(getMyData());
     }
   }, [token, dispatch]);
 
@@ -43,51 +48,51 @@ export function Navbar() {
       </div>
       <div className="flex items-center space-x-4">
         <ModeToggle />
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="relative h-8 w-8 rounded-full">
-              <Avatar className="h-8 w-8">
-                <AvatarImage
-                  src={myInfo ? myInfo.user.photo : ""}
-                  alt={myInfo ? myInfo.user.name : ""}
-                  className="object-cover"
-                />
-                <AvatarFallback>UN</AvatarFallback>
-              </Avatar>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent className="w-56" align="end" forceMount>
-            <Link href="/profile">
-              <DropdownMenuItem>
-                <User className="mr-2 h-4 w-4" />
-                <span>Profile</span>
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <Link
-              href="/login"
-              onClick={() => localStorage.removeItem("token")}>
-              <DropdownMenuItem>
-                <LogOut className="mr-2 h-4 w-4" />
-                <span>Log out</span>
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
-            <Link href="/login">
-              <DropdownMenuItem>
-                <LogIn className="mr-2 h-4 w-4" />
-                <span>Login</span>
-              </DropdownMenuItem>
-            </Link>
-            <DropdownMenuSeparator />
+        {token ? (
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button
+                variant="outline"
+                className="relative h-8 w-8 rounded-full">
+                <Avatar className="h-8 w-8">
+                  <AvatarImage
+                    src={myInfo ? myInfo.user.photo : ""}
+                    alt={myInfo ? myInfo.user.name : ""}
+                    className="object-cover"
+                  />
+                  <AvatarFallback>UN</AvatarFallback>
+                </Avatar>
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+              <Link href="/profile">
+                <DropdownMenuItem>
+                  <User className="mr-2 h-4 w-4" />
+                  <span>Profile</span>
+                </DropdownMenuItem>
+              </Link>
+              <DropdownMenuSeparator />
+              <Link href="/login" onClick={handleLogout}>
+                <DropdownMenuItem>
+                  <LogOut className="mr-2 h-4 w-4" />
+                  <span>Log out</span>
+                </DropdownMenuItem>
+              </Link>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        ) : currentPath === "/login" ? (
+          <Button variant="outline">
             <Link href="/signup">
-              <DropdownMenuItem>
-                <UserPlus className="mr-2 h-4 w-4" />
-                <span>Signup</span>
-              </DropdownMenuItem>
+              <UserPlus />
             </Link>
-          </DropdownMenuContent>
-        </DropdownMenu>
+          </Button>
+        ) : currentPath === "/signup" ? (
+          <Button variant="outline">
+            <Link href="/login">
+              <LogIn />
+            </Link>
+          </Button>
+        ) : null}
       </div>
     </nav>
   );
