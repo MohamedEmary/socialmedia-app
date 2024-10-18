9b68f34f773fb2a1480af9790f67fb6abbc10b22
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-10-18 04:52:47 +0300

    fix: fix some ts issues

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -7,17 +7,16 @@ import reduxStore, { RootState } from "@/lib/Redux/ReduxStore";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-function Page({ params }) {
+function Page({ params: { id } }: { params: { id: string } }) {
   const dispatch = useDispatch<typeof reduxStore.dispatch>();
 
-  const post: PostType = useSelector(
+  const post: PostType | null = useSelector(
     (state: RootState) => state.posts.singlePost
   );
 
   useEffect(() => {
-    const id: string = params.id;
     dispatch(getSinglePost(id));
-  }, []);
+  }, [dispatch, id]);
 
   return (
     <>
