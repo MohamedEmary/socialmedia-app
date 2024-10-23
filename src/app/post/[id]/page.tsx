0b029d7a51537f0606948bb711140a0e02d8f912
Mohamed Emary <64380567+MohamedEmary@users.noreply.github.com>
2024-10-23 19:06:25 +0300

    fix: old post appears for a while when showing another post

--- src/app/post/[id]/page.tsx
+++ src/app/post/[id]/page.tsx
@@ -2,7 +2,7 @@
 import { Post as PostType } from "@/app/types/post.types";
 import Post from "@/components/post/post";
 import { PostSkeleton } from "@/components/post-skeleton";
-import { getSinglePost } from "@/lib/Redux/PostsSlice";
+import { getSinglePost, resetSinglePost } from "@/lib/Redux/PostsSlice";
 import reduxStore, { RootState } from "@/lib/Redux/ReduxStore";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
@@ -22,9 +22,12 @@ function Page({ params: { id } }: { params: { id: string } }) {
     if (!isAuth) {
       router.replace("/login");
     } else {
+      if (post?.id !== id) {
+        dispatch(resetSinglePost());
+      }
       dispatch(getSinglePost(id));
     }
-  }, [dispatch, id, isAuth, router]);
+  }, [dispatch, id, isAuth, router, post?.id]);
 
   return (
     <>
